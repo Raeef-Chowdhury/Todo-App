ed4db5818810847be47f20fa54c7a5fb1f8b874a
Raeef Chowdhury <raeef.chowdhury1@education.nsw.gov.au>
2025-01-06 12:20:24 +1100

    fix item UI, implement add, delete all and count functionality

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 const input = document.querySelector(".todo__input");
 const todoList = document.querySelector(".todo__list");
+const todoCount = document.querySelector(".stat__items--left");
+const todoDeleteAll = document.querySelector(".stat__items--clear");
+
 let todos = [];
 
 input.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     const text = input.value;
-    todos = text.push();
-    todoList.innerHTML = `
-      <li class="todo__item">
-              <div class="todo__item--form">
+
+    // Create new element using createElement
+    const todoItem = document.createElement("li");
+    todoItem.innerHTML = `  <div class="todo__item--box">
                 <label class="todo__label" for="todo__checkbox"></label>
                 <input class="todo__checkbox" type="checkbox" />
-              </div>
-              <p class="todo__text">${text}</p>
-              <ion-icon name="close-outline" class="todo__icon"></ion-icon>
-            </li>
-            `;
+             
+              <p class="todo__text">${text}</p> </div>
+              <ion-icon name="close-outline" class="todo__icon"></ion-icon>`;
+    todoItem.classList.add("todo__item");
+    todoList.appendChild(todoItem);
+    updateCount();
   } else {
+  }
+});
+function updateCount() {
+  const todoItemCount = document.querySelectorAll(".todo__item").length;
+  todoCount.textContent = todoItemCount + " items left";
+}
+todoDeleteAll.addEventListener("click", function () {
+  const todoItemCount = document.querySelectorAll(".todo__item").length;
+  const confirmation = confirm(
+    `Are you sure you want to delete ${todoItemCount} items?`
+  );
+  if (confirmation == true) {
+    todoList.innerHTML = "";
+    updateCount();
   }
 });
